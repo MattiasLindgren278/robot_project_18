a77570429674c944e24217cbfe56ba07df5d4f03
ottoheile <otto@LAPTOP-96E37NQJ.localdomain>
2021-10-06 14:13:03 +0200

    created rotation.c and rotation.h containing rotate function and added them to rotate_test.c

---
+++ rotation.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "brick.h"
+#include "rotation.h"
+
+#define MOTOR_RIGHT OUTA
+#define MOTOR_LEFT  OUTB
+#define MOTOR_BOTH  (MOTOR_LEFT | MOTOR_RIGHT)
+
+void rotate(char direction, int degrees, float speed){
+    tacho_set_speed_sp(MOTOR_BOTH, tacho_get_max_speed(MOTOR_LEFT, 0) * speed);  // Set speed for both motors
+
+    switch(direction){
+        /* Set a new position for both motors
+         * Positive for the left motor relative to the current position
+         * Negative for the right motor relative to the current position
+         * Results in rotation to the right */
+        case('r'):
+            printf("Rotating right %d degrees\n", degrees);
+            tacho_set_position_sp(MOTOR_LEFT, degrees*2);
+            tacho_set_position_sp(MOTOR_RIGHT, -degrees*2);
+            break;
+
+        /* Set a new position for both motors
+         * Positive for the right motor relative to the current position
+         * Negative for the left motor relative to the current position
+         * Results in rotation to the left */
+        case('l'):
+            printf("Rotating left %d degrees\n", degrees);
+            tacho_set_position_sp(MOTOR_RIGHT, degrees*2);
+            tacho_set_position_sp(MOTOR_LEFT, -degrees*2);
+            break;
+
+        /* No valid direction given
+         * Print the error and exit the function */
+        default:
+            printf("ERROR: Parameter direction only accepts argument 'r' or 'l'. Error called from function rotate() located in rotation.c.\n");
+            return;
+    }
+    tacho_run_to_rel_pos(MOTOR_BOTH);   // Run the motors to the newly set positions
+}
--- rotation_test.c
+++ rotation_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include "brick.h"
 #include <unistd.h>
+#include "brick.h"
+#include "rotation.h"
 
 #define MOTOR_RIGHT    	OUTA
 #define MOTOR_LEFT    	OUTB
@@ -12,7 +13,7 @@ int max_speed;
 POOL_T touch_sensor;
 
 int init();
-void rotate(char, int);
+//void rotate(char, int);
 
 int main(void)
 {  
@@ -26,9 +27,9 @@ int main(void)
     touch_sensor = sensor_search(LEGO_EV3_TOUCH);
 	touch_set_mode_touch(touch_sensor);
 
-    rotate('r', 90);
+    rotate('r', 90, 0.1);
     sleep_ms(2000);
-    rotate('l', 90);
+    rotate('l', 90, 0.1);
 
     printf("Press sensor to exit.\n");
     sleep_ms(100);
@@ -67,7 +68,7 @@ int init(){
     }
 }
 
-void rotate(char direction, int degrees){
+/*void rotate(char direction, int degrees){
     tacho_set_speed_sp(MOTOR_BOTH, max_speed * 0.2f);
 
     switch(direction){
@@ -89,4 +90,4 @@ void rotate(char direction, int degrees){
     }
 
     tacho_run_to_rel_pos(MOTOR_BOTH);
-}
+}*/
