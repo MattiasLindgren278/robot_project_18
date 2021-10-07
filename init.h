090a98b45f097df2a39c4ae1b25f71d4d05c0a7f
ottoheile <otto@LAPTOP-96E37NQJ.localdomain>
2021-10-07 17:20:02 +0200

    fixed init.h and changed init_test.c

--- init.h
+++ init.h
@@ -1,5 +1,5 @@
 /**
- * \brief Initializes the brick and checks that all necessary components are connected.
- * \return short: Whether or not the initialization is successful (1 or 0).
+ * \brief   Initializes the brick and checks that all necessary components are connected. 
+ * \return  short: Whether or not the initialization is successful (1 or 0).
  */
-extern short init();
+extern int init(int* max_driving_speed, int* drop_speed);
--- init_test.c
+++ init_test.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "brick.h"
 #include "init.h"
-#include "rotation.h"
+
+int max_driving_speed;
+int max_drop_speed;
 
 int main(void){
-    if(!init())
+    if(!init(&max_driving_speed, &max_driving_speed))
         return 1;
 
-    rotate('l', 45, 0.3f);
+    tacho_reset(OUTA);
+    tacho_set_speed_sp(OUTA, max_driving_speed * 0.1);
+    tacho_run_forever(OUTA);
+    sleep_ms(500);
+    tacho_stop(OUTA);
 
     brick_uninit();
     return 0;
