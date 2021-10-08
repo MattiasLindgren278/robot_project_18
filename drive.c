94c4b8b46da89bcf79aad2ae97c87d0cecad534a
VincentFerrigan <ferrigan@kth.se>
2021-10-08 16:25:03 +0200

    adding drive and from functions

--- drive.c
+++ drive.c
@@ -14,22 +14,20 @@
 #define MOTOR_BOTH  ( MOTOR_LEFT | MOTOR_RIGHT )    /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
 #define PULS_PER_CM 2.13427856547123f               /* Antalet pulsar per mm. 350 puls är ett snurr*/
 
-int max_hastighet;                                  /* Variabel för max hastighet på motorn */
 POOL_T usSensor;
 
 
-void drive(float distance_mm, float speed){
+void drive(float distance_mm, float speed, int max_driving_speed){
 
     usSensor = sensor_search(LEGO_EV3_US);          // finns även fler som börjar på US... in och cm. 
     
     us_set_mode_us_dist_cm(usSensor);               // mm och inte cm
     
-    float   distance =      distance_mm;            //mm
-    float   drive_to_pos = PULS_PER_CM * distance;  //Antalet puls roboten ska köra
+    float   drive_to_pos = PULS_PER_CM * distance_mm;  //Antalet puls roboten ska köra
 
     drive_to_pos = (int)drive_to_pos;               //Byter datatyp från int till float
 
-tacho_set_speed_sp(MOTOR_BOTH, max_hastighet * speed); // Helst 0.5
+tacho_set_speed_sp(MOTOR_BOTH, max_driving_speed * speed); // Helst 0.5
 tacho_set_position(MOTOR_BOTH, 0);                  //sätter start position till 0
 tacho_set_position_sp(MOTOR_BOTH, drive_to_pos);    // sätter antalet pulsar den ska köra 
 tacho_run_to_abs_pos(MOTOR_BOTH);                   // kör motorerna position + position_sp 
---
+++ from_wall.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "brick.h"
+#include "from_wall.h"
+
+#define MOTOR_RIGHT    	OUTA
+#define MOTOR_LEFT    	OUTB
+#define MOTOR_C    	OUTC
+#define MOTOR_D    	OUTD
+#define SENSOR_TOUCH	IN1
+#define SENSOR_2	IN2
+#define SENSOR_3	IN3
+#define SENSOR_4	IN4
+
+#define MOTOR_BOTH  ( MOTOR_LEFT | MOTOR_RIGHT )    /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+
+int max_hastighet;         /* variabel för max hastighet på motorn */
+POOL_T usSensor;
+
+void from_wall(int distance_mm, float speed int max_driving_speed){
+
+    int distance = distance_mm;
+    usSensor = sensor_search(LEGO_EV3_US);  // finns även fler som börjar på US... in och cm. 
+    us_set_mode_us_dist_cm(usSensor);       // mm och inte cm
+    tacho_set_speed_sp(MOTOR_BOTH, max_hastighet * -speed);
+
+    while (sensor_get_value0(usSensor, 0) < distance){
+        tacho_run_forever(MOTOR_BOTH);
+    }
+
+    printf("Stopping\n");
+
+    tacho_stop(MOTOR_BOTH);
+}
