4d99a74c4257fc9f8a619b41061764aba38d1022
Mattias Lindgren <mattias.swenne@gmail.com>
2021-10-09 22:20:05 +0200

    fixed comments

--- drop_book.c
+++ drop_book.c
@@ -12,22 +12,22 @@ POOL_T touchSensor;
 void drop();
 
 void drop(){
-drop_speed = tacho_get_max_speed(MOTOR_DROP, 0) * 0.1f;
+    drop_speed = tacho_get_max_speed(MOTOR_DROP, 0) * 0.1f;//set speed for drop_speed
     
-    /* Set drop speed for medium tacho motor
-    * Start medium tacho motor 
+    /* Set drop speed for motor_drop
+    * Start motor_drop
     * Wait one second
-    * Stop medium tacho motor */
+    * Stop motor_drop */
     printf("Dropping book\n");
     tacho_set_speed_sp(MOTOR_DROP, drop_speed);
     tacho_run_forever(MOTOR_DROP);
     sleep_ms(1000);
     tacho_stop(MOTOR_DROP);
 
-    /* Set speed to reverse dropping mechanism for medium tacho motor
-    * Start the medium tacho motor 
+    /* Set speed * -1 for motor_drop
+    * Start motor_drop
     * Wait one second
-    * Stop the medium tacho motor */
+    * Stop motor_drop */
     printf("Resetting position\n");
     tacho_set_speed_sp(MOTOR_DROP, drop_speed * -1);
     tacho_run_forever(MOTOR_DROP);
