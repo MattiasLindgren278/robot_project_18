bbc8803fc7c35783305985114c9e6d1d1a8da2ae
VincentFerrigan <ferrigan@kth.se>
2021-10-10 19:07:11 +0200

    to wall function

---
+++ to_wall.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "brick.h"
+#include "to_wall.h"
+
+#define MOTOR_RIGHT    	OUTA
+#define MOTOR_LEFT    	OUTB
+#define MOTOR_C    	OUTC
+#define MOTOR_D    	OUTD
+#define SENSOR_TOUCH	IN1
+#define SENSOR_2	IN2
+#define SENSOR_3	IN3
+#define SENSOR_4	IN4
+
+#define MOTOR_BOTH  ( MOTOR_LEFT | MOTOR_RIGHT )    /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+
+POOL_T usSensor;
+
+void to_wall(int distance_mm, float speed){
+
+    usSensor = sensor_search(LEGO_EV3_US);  // finns även fler som börjar på US... in och cm. 
+    us_set_mode_us_dist_cm(usSensor);       // mm och inte cm
+
+    tacho_set_speed_sp(MOTOR_BOTH, tacho_get_max_speed(MOTOR_LEFT, 0) * speed);  // Set speed for both motors
+
+    //while (sensor_get_value0(usSensor, 0) < distance){
+    tacho_run_forever(MOTOR_BOTH);
+    while (sensor_get_value0(usSensor, 0) < distance_mm){
+        sleep_ms(30);
+    }
+
+    printf("Stopping\n");
+
+    tacho_stop(MOTOR_BOTH);
+}
---
+++ to_wall.h
@@ -0,0 +1,7 @@
+/** 
+*   \brief      Drive to the wall. 
+*   \param      distance_mm(float)          Distance to the wall, in millimeters.  
+*   \param      speed(float)                How fast the robot will drive (0 - 1).
+*   \return     N/A 
+*/
+extern void to_wall(int distance_mm, float speed);
