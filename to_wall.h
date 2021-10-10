4b99b21c15f6239d8b8ce8997ec42b8ffa5729c3
VincentFerrigan <ferrigan@kth.se>
2021-10-10 16:01:29 +0200

    updating filestructure

--- to_wall.h
+++
@@ -1,7 +0,0 @@
-/**
-*   \brief      Drive towards the wall. 
-*   \param      distance_to_wall_mm(float)  Distance to wall in millimeters. 
- *  \param      speed(float)                How fast the robot will drive (0 - 1).
-*   \return     N/A  
-*/
-extern void to_wall(int distance_mm, float speed);
