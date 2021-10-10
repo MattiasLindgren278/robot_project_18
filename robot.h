21839ea2c1095972062bccde681a046551ea4f15
ottoheile <otto@LAPTOP-96E37NQJ.localdomain>
2021-10-10 19:24:34 +0200

    fully working robot library

--- robot.h
+++ robot.h
@@ -21,3 +21,26 @@ extern void rotate(char direction, int degrees, float speed);
  * \return  N/A
  */
 extern void drop();
+
+/**
+ * \author Zhonghao Yang, email: zhyang@kth.se
+ */
+extern void find_wall();
+
+/**
+ * \author Vincent Ferrigan, email: ferrigan@kth.se
+ * \brief  Drive forwards or backwards.
+ * \param  distance(int)    Distance to drive in millimeters. Negative values will drive the robot backwards.       
+ * \param  speed(float)     How fast the robot will drive (0 - 1).
+ * \return N/A
+ */
+extern void drive(int distance, float speed);
+
+/**
+ * \author  Vincent Ferrigan, email: ferrigan@kth.se
+ * \brief   Drive away from a wall. 
+ * \param   distance(int)   Distance from the wall in millimeters. 
+ * \param   speed(float)    How fast the robot will drive (0 - 1).
+ * \return  N/A  
+ */
+extern void from_wall(int distance, float speed);
